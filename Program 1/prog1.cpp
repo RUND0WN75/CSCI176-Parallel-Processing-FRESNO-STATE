ab609385d9e84764eac7f3a704fb3b16a83f11f3
RUND0WN75 <42524497+RUND0WN75@users.noreply.github.com>
2023-01-31 22:02:04 -0800

    Latest update

---
+++ Program 1/prog1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <unistd.h> //fork, pipe, wait
+#include <vector>
+using namespace std;
+
+//Recursive fibonacci
+unsigned long long rec(int n) {
+	if (n == 0 || n == 1) return n;
+	else return rec(n - 1) + rec(n - 2);
+}
+
+//Iterative fibonacci
+unsigned long long iter(int n) {
+	unsigned long long prev = 0; cur = 1, next = prev + cur;
+	if (n == 0 || n == 1) return n;
+	for (int i = 2; i < n; i++) {
+		prev = cur;
+		cur = next;
+		next = prev + cur;
+	}
+	return next;
+}
+
+vector<int> proc(2); //Process A and B
+
+int main() {
+	int status;
+	status = pipe(proc);
+	int pid1 = fork();
+
+	if (pid1 == 0) {
+		int pid2 = fork();
+		if (pid2 == 0) iter(1);
+	}
+
+	else rec(1);
+
+	wait(&status);
+	wait(&status);
+}
