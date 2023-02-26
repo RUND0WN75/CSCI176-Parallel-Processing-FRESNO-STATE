4572b61b9672cb21242c7a8db486625d3a63b8e2
RUND0WN75 <42524497+RUND0WN75@users.noreply.github.com>
2023-02-26 11:17:58 -0800

    Changes made on 2/26 @ 11:17am

---
+++ Activity1/Main.java
@@ -0,0 +1,58 @@
+import java.sql.SQLOutput;
+import java.util.ArrayList;
+
+//Thread class using the extends Thread
+public class compThread extends Thread {
+    private int thread_id;
+    double local_sum;
+    int my_first_index;
+    int my_last_index;
+
+    //Constructor
+    compThread(int k, global_sum gs) {
+        this.gs = gs;
+        thread_id = k;
+        System.out.println("Thread for compThread: " + thread_id + " created");
+    }
+
+    //Using run() method
+    void run() {
+        //First calculate my_first_index and my_last_index
+        //Next compute local_sum with a serial operation
+        local_sum = thread_id * 100;
+        System.out.println("Thread " + thread_id + ", local_sum: " + local_sum);
+    }
+}
+
+//Global sum class
+public class globalSum {
+    private static double gsum = 0;
+
+    public synchronized void adding(double local_sum) {
+        gsum += local_sum; //Updating shared variable
+
+        //If at the last thread, display the first global_sum
+        System.out.println("global_sum: " + global_sum)
+    }
+}
+
+public class Main {
+    //Global vector (ArrayList) of size 500 million
+    public static Integer global_size = 500_000_000;
+    public static ArrayList<Integer> arr1 = new ArrayList<Integer>();
+    static global_sum gs = new global_sum();
+
+    public static void main(String[] args) {
+        //Initializing array to i+1
+        for (int i = 0; i < global_size; i++) arr1.add(i+1)
+
+        //Creating 4 computational threads
+        for (int k = 0; k < 4; k++) {
+            compThread t = new compThread(k,gs);
+            t.start();
+        }
+
+        //For general testing purposes
+        System.out.println("Hello world!");
+    }
+}
