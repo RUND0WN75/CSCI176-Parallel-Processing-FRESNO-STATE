a72f4e3b78d677e089c850234184b0e6ff2bb486
RUND0WN75 <42524497+RUND0WN75@users.noreply.github.com>
2023-03-09 15:14:45 -0800

    Starter code

---
+++ Program 3/prog3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+#define SIZE 500000017;
+
+vector<double> globalArea(SIZE);
+
+int main(int argc, char **argv) {
+    cout << "Hello world\n";
+}
